e9263c4196ba7866d9ce9aa9dc62a14cfd5e7434
Everton <everton7romann@gmail.com>
2024-04-02 11:53:45 -0300

    Initial commit

---
+++ server/src/main/java/br/edu/utfpr/pb/tads/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.tads.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
